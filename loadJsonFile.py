22bd5072d36e1398f886e5ea7419d68c821638d3
KooMinJun <mjkoo711@gmail.com>
2018-01-05 14:25:50 -0800

    2018-01-05 Minjun : modifications due to code review

--- loadJsonFile.py
+++ loadJsonFile.py
@@ -5,38 +5,38 @@ import requests
 API_KEY = 'AIzaSyDFtKtm0jnjckE-lB0LkO3SKlWmQIJush8';
 gmaps = googlemaps.Client(key=API_KEY)
 
-# 파일 읽기 -> result.json 로드하기
+# Load file -> load 'result.json'
 with open('result.json') as f:
-	read_data = f.read()
+    read_data = f.read()
 
-# dict type is 'list'
+# Dict type is 'list'
 dict = json.loads(read_data)
 
-# if the information is unknown, save 'unknown'
+# If the information is unknown, save 'unknown'
 for item in dict:
-	try: print(item['name'])
-	except KeyError: print('unknown') 
-	try: print(item['formatted_address'])
-	except KeyError: print('unknown') 
-	try: print(item['geometry']['location']['lat'])
-	except KeyError: print('unknown') 
-	try: print(item['geometry']['location']['lng'])
-	except KeyError: print('unknown') 
-	try: print(item['formatted_phone_number'])
-	except KeyError: print('unknown') 
-	try: print(item['opening_hours']['periods'])
-	except KeyError: print('unknown') 
-	try: print(item['rating'])
-	except KeyError: print('unknown') 
-	try: print(item['website'])
-	except KeyError: print('unknown') 
+    try: print(item['name'])
+    except KeyError: print('unknown') 
+    try: print(item['formatted_address'])
+    except KeyError: print('unknown') 
+    try: print(item['geometry']['location']['lat'])
+    except KeyError: print('unknown') 
+    try: print(item['geometry']['location']['lng'])
+    except KeyError: print('unknown') 
+    try: print(item['formatted_phone_number'])
+    except KeyError: print('unknown') 
+    try: print(item['opening_hours']['periods'])
+    except KeyError: print('unknown') 
+    try: print(item['rating'])
+    except KeyError: print('unknown') 
+    try: print(item['website'])
+    except KeyError: print('unknown') 
 
 	# request photo_url 
-	try: 
-		for photoitem in item['photos']:
-			photo_query = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth={}&photoreference={}&key={}'.format(photoitem["width"], photoitem["photo_reference"], API_KEY)
-			print(photo_query)
-	except KeyError: print('unknown')
+    try: 
+        for photoitem in item['photos']:
+	    photo_query = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth={}&photoreference={}&key={}'.format(photoitem["width"], photoitem["photo_reference"], API_KEY)
+	    print(photo_query)
+    except KeyError: print('unknown')
 
-	print('\n\n')
+    print('\n\n')
 
--- saveJsonFile.py
+++ saveJsonFile.py
@@ -5,27 +5,31 @@ import requests
 API_KEY = 'AIzaSyDFtKtm0jnjckE-lB0LkO3SKlWmQIJush8';
 gmaps = googlemaps.Client(key=API_KEY)
 
-# wework civic : 37.778159, -122.411867
-gmaps_result = gmaps.places_radar(location=(37.778159, -122.411867), radius=500,
-                                 min_price=0, max_price=2, open_now=True, type='cafe')
+# Wework civic : 37.778159, -122.411867
+gmaps_result = gmaps.places_radar(location=(37.778159, -122.411867)
+                                  , radius=500
+                                  , min_price=0
+                                  , max_price=2
+                                  , open_now=True
+                                  , type='cafe')
 
 datalist = []
 
-# gmaps_result has 'results' tag
+# Gmaps_result has 'results' tag
 for place in gmaps_result['results'][0:20]:
 
-    # find place_id
-	place_id = place['place_id']
+    # Find place_id
+    place_id = place['place_id']
 
-    # then find json data from place_id
-	req_query = 'https://maps.googleapis.com/maps/api/place/details/json?placeid={}&key={}'.format(place_id, API_KEY)
+    # Then find json data from place_id
+    req_query = 'https://maps.googleapis.com/maps/api/place/details/json?placeid={}&key={}'.format(place_id, API_KEY)
 
-    # request result recieve and change json
-	req = requests.get(req_query).json()
+    # Request result recieve and change json
+    req = requests.get(req_query).json()
 
 
-	datalist.append(req["result"])
+    datalist.append(req["result"])
 
 
 with open("result.json", "w") as f:
-	f.write(json.dumps(datalist, indent=4))
+    f.write(json.dumps(datalist, indent=4))
