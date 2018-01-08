dfadd68ddad9e4a286862c03acbbfb9e1c7e6d4c
KooMinJun <mjkoo711@gmail.com>
2018-01-08 11:14:00 -0800

    2018-01-08 Minjun : Change data storage method
    - Change data storage method. Because dictionary format is likely to be
    easier to store in the DB.

--- loadJsonFile.py
+++ loadJsonFile.py
@@ -4,36 +4,48 @@ import requests
 with open('result.json') as f:
     read_data = f.read()
 
-dict = json.loads(read_data)
+datainfo = json.loads(read_data)
 
-for item in dict:
-    try: print(item['name'])
-    except KeyError: print('unknown')
-    try: print(", ".join(item['location']['display_address']))
-    except KeyError: print('unknown')
-    try: print(item['coordinates']['latitude'])
-    except KeyError: print('unknown')
-    try: print(item['coordinates']['longitude'])
-    except KeyError: print('unknown')
-    try: print(item['display_phone'])
-    except KeyError: print('unknown')
-    try: print(item['rating'])
-    except KeyError: print('unknown')
-    try: print(item['url'])
-    except KeyError: print('unknown')
+#data_dictionary = dict()
+data = dict()
 
-    try:
-        for openitem in item['hours']:
-            for openitem_detail in openitem['open']:
-                print(openitem_detail['day'])
-                print(openitem_detail['start'])
-                print(openitem_detail['end'])
-    except KeyError: print('unknown')
 
+for item in datainfo:
+    try: data['name'] = item['name']
+    except KeyError: data['name'] = 'unknown'
+    try: 
+        address = ", ".join(item['location']['display_address'])
+        data['address'] = address
+    except KeyError: data['address'] = 'unknown'
+    try: data['latitude'] = item['coordinates']['latitude']
+    except KeyError: data['latitude'] = 'unknown'
+    try: data['longitude'] = item['coordinates']['longitude']
+    except KeyError: data['longitude'] = 'unknown'
+    try: data['phone'] = item['display_phone']
+    except KeyError: data['phone']= 'unknown'
+    try: data['rating'] = item['rating']
+    except KeyError: data['rating'] = 'unknown'
+    try: data['infourl'] = item['url']
+    except KeyError: data['infourl'] = 'unknown' 
+
+    opendata = dict()
+    for openitem in item['hours']:
+        for openitem_detail in openitem['open']:
+            try: opendata['day'] = openitem_detail['day']
+            except KeyError: opendata['day'] = 'unknown'
+            try: opendata['start'] = openitem_detail['start']
+            except KeyError: opendata['start'] = 'unknown'
+            try: opendata['end'] = openitem_detail['end']
+            except KeyError: opendata['end'] = 'unknown'
+    data['openinfo'] = opendata
+
+    photourl = []
     try:
         for photoitem in item['photos']:
-            print(photoitem)
-    except KeyError: print('unknown')
+            photourl.append(photoitem)
+    except KeyError: data['photourl'] = 'unknown'
 
-print('\n\n')
+    data['photourl'] = photourl
+
+print(json.dumps(data, indent=4))
 
