bbd7ab990194fefe8016187af300f4db1bf694f8
KooMinJun <mjkoo711@gmail.com>
2018-01-08 13:29:53 -0800

    2018-01-08 Minjun : Change code for saving data type
    - Change code for saving data type. Because of saving to DB.

--- loadJsonFile.py
+++ loadJsonFile.py
@@ -10,33 +10,49 @@ with open('result.json') as f:
     read_data = f.read()
 
 # Dict type is 'list'
-dict = json.loads(read_data)
+datainfo = json.loads(read_data)
+
+data = dict()
 
 # If the information is unknown, save 'unknown'
-for item in dict:
-    try: print(item['name'])
-    except KeyError: print('unknown') 
-    try: print(item['formatted_address'])
-    except KeyError: print('unknown') 
-    try: print(item['geometry']['location']['lat'])
-    except KeyError: print('unknown') 
-    try: print(item['geometry']['location']['lng'])
-    except KeyError: print('unknown') 
-    try: print(item['formatted_phone_number'])
-    except KeyError: print('unknown') 
-    try: print(item['opening_hours']['periods'])
-    except KeyError: print('unknown') 
-    try: print(item['rating'])
-    except KeyError: print('unknown') 
-    try: print(item['website'])
-    except KeyError: print('unknown') 
+for item in datainfo:
+    try: data['name'] = item['name']
+    except KeyError: data['name'] = 'unknown'
+    try: data['address'] = item['formatted_address']
+    except KeyError: data['address'] = 'unknown'
+    try: data['latitude'] = item['geometry']['location']['lat'] 
+    except KeyError: data['latitude'] = 'unknown' 
+    try: data['longitude'] = item['geometry']['location']['lng']
+    except KeyError: data['longitude'] = 'unknown'  
+    try: data['phone'] = item['formatted_phone_number'] 
+    except KeyError: data['phone'] = 'unknown' 
+    #try: data[] print(item['opening_hours']['periods'])
+    #except KeyError: print('unknown') 
+    try: data['rating'] = item['rating'] 
+    except KeyError: data['rating'] = 'unknown' 
+    try: data['website'] = item['website']
+    except KeyError: data['website'] = 'unknown'
 
-	# request photo_url 
+    opendatalist = []
+    for openitem in item['opening_hours']['periods']:
+        opendata = dict()
+        try: opendata['day'] = openitem['open']['day']
+        except KeyError: opendata['day'] = 'unknown'
+        try: opendata['start'] = openitem['open']['time']
+        except KeyError: opendata['start'] = 'unknown'
+        try: opendata['end'] = openitem['close']['time']
+        except KeyError: opendata['end'] = 'unknown'
+        opendatalist.append(opendata)
+    data['openinfo'] = opendatalist
+
+
+
+    photourl = []
     try: 
         for photoitem in item['photos']:
-	    photo_query = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth={}&photoreference={}&key={}'.format(photoitem["width"], photoitem["photo_reference"], API_KEY)
-	    print(photo_query)
-    except KeyError: print('unknown')
-
-    print('\n\n')
+            photo_query = 'https://maps.googleapis.com/maps/api/place/photo?maxwidth={}&photoreference={}&key={}'.format(photoitem["width"], photoitem["photo_reference"], API_KEY)
+            photourl.append(photo_query)
+    except KeyError: data['photourl'] = 'unknown'
+    data['photourl'] = photourl
+    print(json.dumps(data, indent=4))
 
