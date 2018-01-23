2b34f9011c84c09f73cd9764bc668bfa3151d220
khc196 <khc146@gmail.com>
2018-01-23 02:30:47 +0000

    2018-01-22 Hwancheol : Update app.py for running on https

--- app.py
+++ app.py
@@ -152,4 +152,4 @@ if __name__ == '__main__':
 
     print("Starting app on port %d" % port)
 
-    app.run(debug=False, port=port, host='0.0.0.0')
+    app.run(debug=False, port=port, host='0.0.0.0', ssl_context=('cert.pem', 'key.pem'))
