c3b264c197f5eecd163253b2e941526b435c21a6
Gihyeon Yang <dexterastin@gmail.com>
2018-01-22 18:46:07 -0800

    Remove extra line

--- static/js/initCardMapView.js
+++ static/js/initCardMapView.js
@@ -134,7 +134,6 @@ window.onload = function () {
                             activeCard.trigger("click");
                         }
                     }, 500);
-
                     drop();
                 }
             }
