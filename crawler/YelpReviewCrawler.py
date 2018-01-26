517934403cd6d36f708317fe913c75bf85ad9fc3
khc196 <khc146@gmail.com>
2018-01-26 20:45:13 +0000

    2018-01-26 Hwancheol : import needed module

--- crawler/YelpReviewCrawler.py
+++ crawler/YelpReviewCrawler.py
@@ -1,2 +1,14 @@
 ## !/usr/bin/env python
 from bs4 import BeautifulSoup
+import requests, json
+
+try:
+    # For Python 3.0 and later
+    from urllib.error import HTTPError
+    from urllib.parse import quote
+    from urllib.parse import urlencode
+except ImportError:
+    # Fall back to Python 2's urllib2 and urllib
+    from urllib2 import HTTPError
+    from urllib import quote
+    from urllib import urlencode
