6377ceaad0bd2031a802bc716c0ecf225b8f3982
khc196 <khc146@gmail.com>
2018-01-29 17:52:12 -0800

    2018-01-29 Hwancheol : Implement a review crawler

--- crawler/YelpReviewCrawler.py
+++ crawler/YelpReviewCrawler.py
@@ -1,6 +1,6 @@
 ## !/usr/bin/env python
 from bs4 import BeautifulSoup
-import requests, json
+import requests, json, pymysql
 
 try:
     # For Python 3.0 and later
@@ -12,3 +12,30 @@ except ImportError:
     from urllib2 import HTTPError
     from urllib import quote
     from urllib import urlencode
+
+sql_conn = pymysql.connect(host='localhost', user='gaeul', password='alpha', db='CAFE', charset='utf8mb4')
+curs = sql_conn.cursor(pymysql.cursors.DictCursor)
+
+query = "select * from CAFES"
+curs.execute(query)
+data = curs.fetchall()
+for row in data :
+    i = 0
+    while True : 
+        crawling_url = row['yelpurl'] 
+        soup = BeautifulSoup(crawling_url, "lxml")
+        try :
+            index = i * 20
+            plain_text = requests.get(crawling_url + "?start=" + str(index)).text
+            soup = BeautifulSoup(plain_text, "lxml")
+            # parsing reviews
+            reviews = soup.select('p[lang|=en]')
+            for review in reviews :
+                # TODO : Remove later
+                print(review)
+                print('=============================================================================================')
+        except Exception as ex : 
+            print(ex)
+            break
+    i = i + 1
+    # TODO : Insert review to DB
