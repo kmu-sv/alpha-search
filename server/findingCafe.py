c21949e645d14446b984eb873a918e9a566bbf54
Gaeul <gaeulgo2@gmail.com>
2018-01-29 16:22:06 -0800

    2018-01-29 Gaeul : changed referring to the Github comment
    changed indentation, deleted several 'print'

--- server/findingCafe.py
+++ server/findingCafe.py
@@ -12,10 +12,10 @@ open_cafe_list = []
 # connect to DB
 def getConnection():
     return pymysql.connect(host='localhost',
-    user='gaeul',
-    password='alpha',
-    db='CAFE',
-    charset='utf8mb4')
+                            user='gaeul',
+                            password='alpha',
+                            db='CAFE',
+                            charset='utf8mb4')
 
 # inquiry data from connected DB 
 def getTable():
@@ -42,8 +42,6 @@ def findOpenCafes(cafes):
     day_to_find = now_day + 7 # the number '7' is to match with attribute 
     #loop for each cafe in cafe list
     for cafe in cafes :
-        print (cafe)
-        print (cafe[1],cafe[day_to_find])
         if cafe[day_to_find] == 'NULL' :
             continue
         else :
@@ -57,8 +55,6 @@ def findOpenCafes(cafes):
                 ((close_hour == now_hour and close_minute >= now_minute) or (close_hour > now_hour))) :
                 open_cafe_list.append(cafe[1])
     return open_cafe_list
-
-
 
 if __name__ == "__main__":
     cafe_list = getTable()
