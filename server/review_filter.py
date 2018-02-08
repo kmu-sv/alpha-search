2485a0e1a5ffd3cebbaaa556b03fc698f9b187dd
khc196 <khc146@gmail.com>
2018-02-07 23:55:18 -0800

    2018-02-07 Hwancheol : fetch and parsing

--- server/review_filter.py
+++ server/review_filter.py
@@ -3,3 +3,14 @@ import json, pymysql
 # Connect to MySQL and Generete dictionary cursor from connection
 sql_conn = pymysql.connect(host='localhost', user='gaeul', password='alpha', db='CAFE', charset='utf8mb4')
 curs = sql_conn.cursor(pymysql.cursors.DictCursor)
+# Fetch data from DB
+query = "SELECT cafe_id, reviews from CAFES_REVIEWS"
+curs.execute(query)
+reviews_list = curs.fetchall()
+
+for row in reviews_list :
+    reviews = row['reviews']
+    review_list = reviews.split("#tag#")
+    filtered_list = list()
+    for review in review_list :
+        filtered_list.append(review)
