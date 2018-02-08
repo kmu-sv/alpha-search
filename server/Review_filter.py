303d648348a203881ae6d1d5af0f172297c76ed3
khc196 <khc146@gmail.com>
2018-02-08 11:04:42 -0800

    Update Review_filter.py

--- server/Review_filter.py
+++ server/Review_filter.py
@@ -3,14 +3,14 @@ import json, pymysql
 # Connect to MySQL and Generete dictionary cursor from connection
 sql_conn = pymysql.connect(host='localhost', user='gaeul', password='alpha', db='CAFE', charset='utf8mb4')
 curs = sql_conn.cursor(pymysql.cursors.DictCursor)
-# Fetch data from DB
-query = "SELECT cafe_id, reviews from CAFES_REVIEWS"
-curs.execute(query)
 
-getFilteredReviews() :
+setFilteredReviews() :
+    # Fetch data from DB
+    fetch_query = "SELECT cafe_id, reviews from CAFES_REVIEWS"
+    curs.execute(fetch_query)
     reviews_list = curs.fetchall()
-    dict_list = list()
-
+    # Filter review and Update table
+    insert_query = """UPDATE CAFES_REVIEWS_FILTERED SET reviews_filtered = %s WHERE cafe_id = %s"""
     for row in reviews_list :
         reviews = row['reviews']
         review_list = reviews.split("#tag#")
@@ -21,5 +21,9 @@ getFilteredReviews() :
                 or "wifi" in review_lower \
                 or "outlet" in review_lower :           
                 filtered_list.append(review)
-        dict_list.append(row['cafe_id'] : str(filtered_list))
-    return json.dumps(dict_list)
+        curs.execute(insert_query, (str(filtered_list), str(row['cafe_id'])))
+getFilteredReviews() :
+    fetch_query = "SELECT * from CAFES_REVIEWS_FILTERED"
+    curs.execute(fetch_query)
+    filtered_reviews_list = curs.fetchall()
+    return filtered_reviews_list
